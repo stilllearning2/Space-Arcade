daca50db637fb1fb97123f76abf138afc5caec0b
Craig Alford <crglfrd@gmail.com>
2019-02-07 20:23:20 -0800

    Upload src files

---
+++ src/program.js
@@ -0,0 +1,100 @@
+/*global window: false */
+/*global document: false */
+/*jslint node: true */
+"use strict";
+
+// Arrow key codes
+var UP = 38;
+var DOWN = 40;
+var RIGHT = 39;
+var LEFT = 37;
+var UFOUP = 119;
+var UFODOWN = 122;
+
+// Velocity
+var velocity = 2;
+
+// DOM elements
+var torpedo = document.querySelector("#torpedo");
+var startBtn = document.querySelector("#start");
+var fireBtn = document.querySelector("#fire");
+var introScreen = document.querySelector("#introScreen");
+var gameScreen = document.querySelector("#gameScreen");
+
+// rocket object
+var rocket = {
+        img: document.querySelector("#rocket"),
+        x: 490,
+        y: 390,
+        width: 100
+    };
+// ufo object
+var ufo = {
+        img: document.querySelector("#ufo"),
+        x: 0,
+        y: 0,
+        width: 100
+    };
+
+function startGameHandler() {
+    // Hide the intro screen, show the game screen
+    introScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    rocket.img.style.display = "block";
+    ufo.img.style.display = "block";
+}
+
+function fireTorpedoHandler() {
+    // Fire the photon torpedo!
+    // CSS animation occurs whenever torpedo
+    // 'left' property changes value
+    torpedo.style.visibility = "visible";
+    torpedo.style.left = (rocket.x - 200) + "px";
+}
+
+function render() {
+    // position objects on the screen
+    rocket.img.style.left = rocket.x + "px";
+    rocket.img.style.top = rocket.y + "px";
+    torpedo.style.left = (rocket.x + 10) + "px";
+    torpedo.style.top = (rocket.y + 8) + "px";
+    torpedo.style.visibility = "hidden";
+    if (torpedo.style.left < ufo.style.left + 100) {
+        if (torpedo.style.top < ufo.style.top + 100 && torpedo.style.top > ufo.style.top) {
+            // audio explosion
+            torpedo.style.display = "none";
+            ufo.style.display = "none";
+        }
+    }
+}
+
+function keydownHandler(event) {
+    // handle user keyboard input
+    if (event.keyCode === UP) {
+        rocket.y -= velocity;
+    }
+    if (event.keyCode === LEFT) {
+        rocket.x -= velocity;
+    }
+    if (event.keyCode === DOWN) {
+        rocket.y += velocity;
+    }
+    if (event.keyCode === RIGHT) {
+        rocket.x += velocity;
+    }
+    if (event.keyCode === UFOUP) {
+        ufo.y -= velocity;
+    }
+    if (event.keyCode === UFODOWN) {
+        ufo.y += velocity;
+    }
+
+    render();
+}
+
+// Initialize objects on the screen
+startBtn.addEventListener("click", startGameHandler, false);
+fireBtn.addEventListener("click", fireTorpedoHandler, false);
+window.addEventListener("keydown", keydownHandler, false);
+
+render();
